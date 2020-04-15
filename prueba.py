f77d9af6d6e8a3213e370a0a2cd65ca73dda4320
MsAlderson <cerquera.e@gmail.com>
2020-04-14 21:06:17 -0500

    este cambio no sirvió para lo que quería, pero sí para saber que debo cambiar de rama antes de querer guardar cualquier cambio

--- prueba.py
+++ prueba.py
@@ -4,3 +4,6 @@ print ("a ver qué pasa con tro commit")
 
 #cambiaré a la rama master, y guardaré ahí a ver si aparece el cambio en la otra cuenta
 print (2 * 9)
+
+#volveré a la nueva rama y haré algo con lo del merge upstream/master
+print ("hola hola")
